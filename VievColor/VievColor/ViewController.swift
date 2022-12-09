18b9370d7f037f2d7de43d66edc22ccf277ffba9
Bohdan Plakuschiy <plakushiy.bogdan@gmail.com>
2022-12-09 14:22:30 +0200

    var and func finished

--- VievColor/VievColor/ViewController.swift
+++ VievColor/VievColor/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var redValueText: UILabel!
+    @IBOutlet weak var greenValueText: UILabel!
+    @IBOutlet weak var blueValueText: UILabel!
+    @IBOutlet weak var alphaValueText: UILabel!
+    @IBOutlet weak var vievColor: UIView!
+    
+    @IBOutlet weak var redValueSlider: UISlider!
+    @IBOutlet weak var greenValueSlider: UISlider!
+    @IBOutlet weak var blueValueSlider: UISlider!
+    @IBOutlet weak var alphaValueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func redSlider(_ sender: Any) {
+    }
+    
+    
+    @IBAction func greenSlider(_ sender: Any) {
+    }
+    
+    
+    @IBAction func blueSlider(_ sender: Any) {
+    }
+    
+    @IBAction func alphaSlider(_ sender: Any) {
+    }
+    
+    @IBAction func minPressedButton(_ sender: Any) {
+    }
+    
+    @IBAction func mediumPressedButton(_ sender: Any) {
+    }
+    
+    @IBAction func maxPressedButton(_ sender: Any) {
+    }
+    
 }
 
