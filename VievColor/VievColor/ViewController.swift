7a2ba516d93ffc0ef3eed8064d5860c291325394
Bohdan Plakuschiy <plakushiy.bogdan@gmail.com>
2022-12-09 15:50:04 +0200

    finished my version

--- VievColor/VievColor/ViewController.swift
+++ VievColor/VievColor/ViewController.swift
@@ -23,32 +23,82 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redValueSlider.maximumValue = 255
+        greenValueSlider.maximumValue = 255
+        blueValueSlider.maximumValue = 255
+        alphaValueSlider.maximumValue = 255
+        redValueSlider.minimumValue = 0
+        greenValueSlider.minimumValue = 0
+        blueValueSlider.minimumValue = 0
+        alphaValueSlider.minimumValue = 0
+        startValueSlider()
+        textValueSliders()
     }
-
 
     @IBAction func redSlider(_ sender: Any) {
+        textValueSliders()
+        toneControl()
     }
-    
     
     @IBAction func greenSlider(_ sender: Any) {
+        textValueSliders()
+        toneControl()
     }
     
-    
     @IBAction func blueSlider(_ sender: Any) {
+        textValueSliders()
+        toneControl()
+        
     }
     
     @IBAction func alphaSlider(_ sender: Any) {
+        textValueSliders()
+        toneControl()
     }
     
     @IBAction func minPressedButton(_ sender: Any) {
+        redValueSlider.value = redValueSlider.minimumValue
+        greenValueSlider.value = greenValueSlider.minimumValue
+        blueValueSlider.value = blueValueSlider.minimumValue
+        alphaValueSlider.value = alphaValueSlider.minimumValue
+        textValueSliders()
+        toneControl()
     }
     
     @IBAction func mediumPressedButton(_ sender: Any) {
+        startValueSlider()
+        textValueSliders()
+        toneControl()
     }
     
     @IBAction func maxPressedButton(_ sender: Any) {
+        redValueSlider.value = redValueSlider.maximumValue
+        greenValueSlider.value = greenValueSlider.maximumValue
+        blueValueSlider.value = blueValueSlider.maximumValue
+        alphaValueSlider.value = alphaValueSlider.maximumValue
+        textValueSliders()
+        toneControl()
     }
     
+    func toneControl() {
+        vievColor.backgroundColor = UIColor(red: CGFloat(redValueSlider.value / redValueSlider.maximumValue),
+                                            green: CGFloat(greenValueSlider.value / greenValueSlider.maximumValue),
+                                            blue: CGFloat(blueValueSlider.value / blueValueSlider.maximumValue),
+                                            alpha: CGFloat(alphaValueSlider.value / alphaValueSlider.maximumValue))
+    }
+    
+    func startValueSlider() {
+                redValueSlider.value = (redValueSlider.maximumValue - redValueSlider.minimumValue) / 2 + redValueSlider.minimumValue
+                greenValueSlider.value = (greenValueSlider.maximumValue - greenValueSlider.minimumValue) / 2 +  greenValueSlider.minimumValue
+                blueValueSlider.value = (blueValueSlider.maximumValue - blueValueSlider.minimumValue) / 2 + redValueSlider.minimumValue
+                alphaValueSlider.value = (alphaValueSlider.maximumValue - alphaValueSlider.minimumValue) / 2 + alphaValueSlider.minimumValue
+    }
+    
+    func textValueSliders() {
+        redValueText.text = "\(Int(redValueSlider.value))"
+        greenValueText.text = "\(Int(greenValueSlider.value))"
+        blueValueText.text = "\(Int(blueValueSlider.value))"
+        alphaValueText.text = "\(Int(alphaValueSlider.value))"
+    }
 }
 
